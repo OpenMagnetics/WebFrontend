4f7da58ff82591f60097a44fdb8a81a477f71af5
AlfVII <Alfonso_VII@hotmail.com>
2023-07-23 23:42:46 +0200

    Fixed typo

--- src/views/ToolSelection.vue
+++ src/views/ToolSelection.vue
@@ -47,7 +47,7 @@ export default {
                     <div class="text-white my-5 p-2 text-center offset-1 col-sm-10 col-3-md col-lg-3 bg-light rounded-4">
                         <h2 class="">Wire tools</h2>
                         <div class="" aria-label="Group with wire button">
-                            <button v-resize-text="{ratio:0.7, minFontSize: '14px', maxFontSize: '20px', delay: 20}" data-test-id="ToolSelection-find-cots-wire-button" class="m-2 btn btn-primary disabled">Find COTS wire</buttontton>
+                            <button v-resize-text="{ratio:0.7, minFontSize: '14px', maxFontSize: '20px', delay: 20}" data-test-id="ToolSelection-find-cots-wire-button" class="m-2 btn btn-primary disabled">Find COTS wire</button>
                             <button v-resize-text="{ratio:0.7, minFontSize: '14px', maxFontSize: '20px', delay: 20}" data-test-id="ToolSelection-cross-reference-wire-button" class="m-2 btn btn-primary disabled">Cross reference wire</button>
                             <button v-resize-text="{ratio:0.7, minFontSize: '14px', maxFontSize: '20px', delay: 20}" data-test-id="ToolSelection-customize-wire-button" class="m-2 btn btn-primary disabled">Customize wire</button>
                         </div>
