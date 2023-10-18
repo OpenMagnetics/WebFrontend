a4b01e121535fd27081008e104e9fe02fc6a8937
AlfVII <Alfonso_VII@hotmail.com>
2023-10-18 11:09:40 +0200

    Fixed bug in STP export in core

--- src/views/SlugLoader.vue
+++ src/views/SlugLoader.vue
@@ -3,7 +3,7 @@ import OperationPoint from '/src/views/OperationPoint.vue'
 import Core from '/src/views/Core.vue'
 import { useCommonStore } from '/src/stores/waveform'
 import { useCoreStore } from '/src/stores/core'
-import * as Utils from '/src/assets/js/utils.js'
+import { deepCopy } from '/src/assets/js/utils.js'
 
 </script>
 <script>
@@ -58,9 +58,26 @@ export default {
                     if (response.data == null)
                         this.$router.push('/');
                     else {
+
                         this.$userStore.globalCore = response.data["element"]
-                        Utils.getCoreParameters(this.$userStore, () => {this.coreStore.setDataReadOnly(true); this.coreLoaded = true;}, () => {})
-                        
+                        this.$mkf.ready.then(_ => {
+                            const aux = deepCopy(this.$userStore.globalCore);
+                            aux['geometricalDescription'] = null;
+                            aux['processedDescription'] = null;
+
+                            if (typeof aux['functionalDescription']['shape'] === 'string' || aux['functionalDescription']['shape'] instanceof String) {
+                                aux['functionalDescription']['shape'] = findCoreShape(this.$dataCacheStore, aux['functionalDescription']['shape']);
+                            }
+
+                            var core = JSON.parse(this.$mkf.calculate_core_data(JSON.stringify(aux), false));
+
+                            this.$userStore.globalCore = core;
+                            this.coreStore.setDataReadOnly(true);
+                            this.coreLoaded = true;
+                        }).catch(error => { 
+                            console.error(error)
+                        });
+
                     }
 
                 })
