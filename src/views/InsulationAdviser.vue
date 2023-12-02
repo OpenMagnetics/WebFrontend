b1a29ee93c22b506a283399fa884c345b7428b19
AlfVII <Alfonso_VII@hotmail.com>
2023-12-02 20:14:02 +0100

    Insulation Adviser done

---
+++ src/views/InsulationAdviser.vue
@@ -0,0 +1,55 @@
+<script setup>
+import Header from '/src/components/Header.vue'
+import Footer from '/src/components/Footer.vue'
+import Storyline from '/src/components/Storyline.vue'
+import { toTitleCase } from '/src/assets/js/utils.js'
+
+import GenericTool from '/src/components/Toolbox/GenericTool.vue'
+
+</script>
+
+<script>
+export default {
+    data() {
+        const currentStoryline = {
+            "insulationRequirements": {
+                title: "Insulation Requirements",
+            },
+        };
+
+        return {
+            currentStoryline,
+            updateStoryline: 0,
+        }
+    },
+    methods: {
+    },
+    mounted() {
+    },
+    created() {
+    },
+}
+</script>
+
+<template>
+    <GenericTool
+        :toolLabel="'insulationAdviser'"
+        :currentStoryline="currentStoryline"
+        :dataTestLabel="'InsulationAdviser'"
+    />
+</template>
+
+
+<style lang="css">
+
+.tool {
+    min-height: 84vh;
+    overflow: hidden;
+}
+.storyline {
+    max-height: auto;
+    position: fixed;
+    overflow: hidden;
+}
+
+</style>
